52e63988bff05e7dda9e57dfe817ee2b9976a44d
ValiusSloth <valius.kaciurin@gmail.com>
2020-06-22 17:20:33 +0300

    created active currency selection component

---
+++ src/components/currency-select.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+class CurrencySelect extends React.Component
+{
+    /**
+     * Callback for currency selection
+     * 
+     * @callback onCurrencySelect
+     * @param {string} key 
+     */
+
+    /**
+     * @param {Object} props
+     * @param {Array}  props.currencies available currencies
+     * @param {CurrencySelect~onCurrencySelect} props.onCurrencySelect
+     */
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            selected: null
+        }
+    }
+
+    render()
+    {
+        return (
+            <div className="currency-select-component">
+                <label>Add currencies to compare:</label>
+                <select onChange={(e) => this.setState({ selected: e.target.value })}>
+                {this.props.currencies ? this.props.currencies.map(currency => (
+                    <option key={currency}>{currency}</option>
+                )) : null}
+                </select>
+                <button onClick={() => this.props.onCurrencySelect(this.state.selected)}>Add</button>
+            </div> 
+        )
+    }
+}
+
+export default CurrencySelect
--- src/components/exchange-rates.js
+++ src/components/exchange-rates.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BtcInput from './btc-input'
+import CurrencySelect from './currency-select'
 
 class ExchangeRates extends React.Component
 {
@@ -12,7 +13,15 @@ class ExchangeRates extends React.Component
     {
         super(props)
         this.state = {
-            amount: null
+            amount: null,
+            activeCurrencies: [],
+        }
+    }
+
+    addActiveCurrency(currency)
+    {
+        if (this.state.activeCurrencies.indexOf(currency) === -1) {
+            this.setState({ activeCurrencies: this.state.activeCurrencies.push(currency) })
         }
     }
 
@@ -21,6 +30,8 @@ class ExchangeRates extends React.Component
         return (
             <div className="exchange-rates-component">
                 <BtcInput onInputChange={value => this.setState({ amount: value })}/>
+                <CurrencySelect currencies={this.props.data ? Object.keys(this.props.data.bpi) : null} 
+                                onCurrencySelect={value => this.addActiveCurrency(value)}/>
             </div>
         )
     }
