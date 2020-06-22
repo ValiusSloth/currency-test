c21215649950cc4f9483131b0fb0e5d65f06cb55
ValiusSloth <valius.kaciurin@gmail.com>
2020-06-22 16:48:49 +0300

    added jsdoc

--- src/components/btc-input.js
+++ src/components/btc-input.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
+/**
+ * Callback for change in input field
+ * 
+ * @callback onInputChange
+ * @param {string} value 
+ */
+
+/**
+ * @param {Object} props
+ * @param {BtcInput~onInputChange} props.onInputChange
+ */
 const BtcInput = ({ onInputChange }) => (
     <div className="btc-input-component">
         <label>BTC amount:</label>
