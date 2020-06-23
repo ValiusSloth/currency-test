6ed566fa0cbf3ef0831941bb2a170c200b27b24e
ValiusSloth <valius.kaciurin@gmail.com>
2020-06-23 14:08:22 +0300

    refactored component to use hooks for readability

--- src/CurrencyWidget.js
+++ src/CurrencyWidget.js
@@ -1,43 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ExchangeRates from './components/exchange-rates'
 
-class CurrencyWidget extends React.Component
-{
-    constructor(props)
-    {
-        super(props)
-        this.state = {
-            data: null,
-        }
-    }
 
-    componentDidMount()
-    {
-        this.fetchData()
-    }
+const CurrencyWidget = () => {
+    let [state, setState] = useState({ data: null });
+    let [delay, setDelay] = useState(1000)
 
-    async fetchData()
-    {
+    useEffect(()=>{
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, delay)
+
+        return () => clearInterval(intervalId);
+    }, [delay]);
+
+    const fetchData = () => {
+        console.log(state.timer)
         fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
         .then(response => response.json())
-        .then(data => this.setState({ data }))
-        .then(this.interval = setInterval(() => this.fetchData(), 60000))
+        .then(data => setState({ data }))
+        .then(setDelay(60000))
         .catch((error) => {
             console.log(error)
         })
     }
 
-    componentWillUnmount()
-    {
-        clearInterval(this.interval)
-    }
-
-    render()
-    {
-        return (
-            <ExchangeRates data={this.state.data}/>
-        )
-    }
+    return (
+        <ExchangeRates data={state.data}/>
+    )
 }
 
 export default CurrencyWidget
