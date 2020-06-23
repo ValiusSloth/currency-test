91c02233dcf8d052d504b14323ab68763dcc6ea4
ValiusSloth <valius.kaciurin@gmail.com>
2020-06-23 13:26:23 +0300

    added jsdoc

--- src/components/currency-list.js
+++ src/components/currency-list.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import CurrencyItem from './currency-item'
 
+/**
+ * Callback for currency item button click
+ * 
+ * @callback onCurrencyRemoveClick
+ * @param {string} currency 
+ */
+
+/**
+ * 
+ * @param {Object} props
+ * @param {Array}  props.currencies active currencies
+ * @param {Object} props.currencyData contains currency data
+ * @param {Int}    props.amount selected currency amount
+ * @param {CurrencyList~onCurrencyRemoveClick}
+ */
 const CurrencyList = ({ currencies, currencyData, amount, onCurrencyRemoveClick }) => (
     <div className="component-currency-list">
         {currencies.map(currency => (
